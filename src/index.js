e05cdb651f7bd6b53dfab2d1ee02c49f28ee3f32
frenki1231 <70661525+frenki1231@users.noreply.github.com>
2020-09-15 22:09:54 +0300

    fix: add the solution of the problem

--- src/index.js
+++ src/index.js
@@ -38,8 +38,39 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let mus = expr.split("**********");
+    let musWord=[];
+    let result=[];   
+    let letterBin="";
+    let str="";
+    let count=0;
+    function letter(str){
+     let result="";   
+     for (let i=str.length-1; i>=0; i-=2){
+         if (str[i-1]+str[i]=="10"){
+             result+=".";
+         }else if (str[i-1]+str[i]=="11"){
+             result+="-";
+         }
+     }
+     let newResult=result.split("").reverse().join("");
+     return MORSE_TABLE[newResult];
+    }
+    for (let i=0; i<mus.length; i++){
+        for (let j=0; j<mus[i].length; j++){
+         count++;
+         str+=mus[i][j];
+         if (count==10){
+                count=0;
+                musWord.push(letter(str));
+                str="";
+         }
+      }
+      result.push(musWord.join(""));
+      musWord.splice(0);
+    }
+    return result.join(" ");
+ }
 
 module.exports = {
     decode
